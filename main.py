61eb9c02800c31bd46dc1fb6eaf57971bdfbce6b
Airi Taukurade <34795565+Taukurade@users.noreply.github.com>
2021-04-11 15:55:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,137 @@
+from flask import Flask, request, Response
+from flask import render_template
+import json
+import asyncio
+import requests
+app = Flask(__name__)
+membs=""
+
+
+members=[
+    {
+        'name':'member1',
+        'score':2211,
+        'a_urls':'https://as2.ftcdn.net/jpg/01/17/16/11/500_F_117161164_mBhUQUTj1vXMJYlBLVBnwgaoeNihNv00.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        },
+        {
+            'name':'man',
+            'color':'#2595a8'
+        },
+        {
+            'name':'dude you...',
+            'color':'#b03030'
+        }
+        ]
+
+    },
+    {
+        'name':'member2',
+        'score':0,
+        'a_urls':'https://floritime.ru/userfiles/shop/medium/2/12863_kashpo-nieuwkoop-alegria-random-square-rusted-.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        },
+        {
+            'name':'YOLOOOOOOO',
+            'color':'#25f5a8'
+        },
+        {
+            'name':'dude you...',
+            'color':'#b03030'
+        }
+        ]
+
+    },
+    {
+        'name':'member4',
+        'score':140,
+        'a_urls':'https://forums.odforce.net/uploads/monthly_2019_05/l_pt_02510.jpg.beee0f7bfd018892e162cc146d08486c.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        }
+        ]
+
+    },
+    {
+        'name':'member1',
+        'score':-2,
+        'a_urls':'https://as2.ftcdn.net/jpg/01/17/16/11/500_F_117161164_mBhUQUTj1vXMJYlBLVBnwgaoeNihNv00.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        },
+        {
+            'name':'man',
+            'color':'#2595a8'
+        },
+        {
+            'name':'dude you...',
+            'color':'#b03030'
+        }
+        ]
+
+    },
+    {
+        'name':'member2',
+        'score':22,
+        'a_urls':'https://floritime.ru/userfiles/shop/medium/2/12863_kashpo-nieuwkoop-alegria-random-square-rusted-.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        },
+        {
+            'name':'YOLOOOOOOO',
+            'color':'#25f5a8'
+        },
+        {
+            'name':'dude you...',
+            'color':'#b03030'
+        }
+        ]
+
+    },
+    {
+        'name':'member4',
+        'score':14,
+        'a_urls':'https://forums.odforce.net/uploads/monthly_2019_05/l_pt_02510.jpg.beee0f7bfd018892e162cc146d08486c.jpg',
+        'roles':
+        [
+        {
+            'name':'muted',
+            'color':'#a1a1a1'
+        }
+        ]
+
+    }
+    ]
+def srt(ex):
+    return ex.get('score')
+@app.route('/webhook/leaderboardoikjrngWPOIURGBLREGOVCQNHREWQLIGFPWQOHRUCGTEQR', methods=['POST'])
+def respond():
+    global members
+    members=request.json
+    return Response(status=200)
+@app.route('/')
+def hello_world():
+
+    global members
+    members.sort(key=srt,reverse=True)
+    return render_template('index.html',members=members)
+
+if __name__ == "__main__":
+    app.run(host="192.168.0.102",port=4402)
---
+++ sockbot.py
@@ -0,0 +1,128 @@
+import discord
+from discord.ext import commands,tasks
+from discord.utils import get
+import random
+import sqlite3
+import json
+from datetime import datetime as dt
+import threading as tr
+import os
+import asyncio
+import requests
+
+mlist=[]
+
+
+
+
+#const
+
+
+
+cn = sqlite3.connect("secret") # или :memory:
+cur = cn.cursor()
+#y = tr.Thread(target=get_members,daemon=True)
+
+#l = tr.Thread(target=run, daemon=True)
+cur.execute("select token, default_cfg from 'bot data'")
+token = cur.fetchone()[0]
+intents = discord.Intents().all()
+
+bot = commands.Bot(command_prefix="-",intents=intents)
+bot.remove_command("help")
+
+def get_members():
+    mlistx=[]
+    global mlist
+    for member in bot.get_guild(806209406860656672).members:
+        cur.execute(f"select score from 'lb' where id={member.id}")
+        if cur.fetchone() == None:
+            cur.execute(f"insert into lb values ({member.id}, 0)")
+            cur.execute(f"select score from 'lb' where id={member.id}")
+            score=cur.fetchone()[0]
+        else:
+            cur.execute(f"select score from 'lb' where id={member.id}")
+            score=cur.fetchone()[0]
+        mname=f"{member.name}#{member.discriminator}"
+        aurl=(str(member.avatar_url))
+        rls=[]
+        for role in member.roles:
+            if role.name == "@everyone":
+                continue
+            rname=str(role.name)
+            rcol=str(role.color)
+            rls.append({'name':rname,'color':rcol})
+        x={'name':mname,'a_urls':aurl,'score':score,'roles':rls}
+        mlistx.append(x)
+    mlist=json.dumps(mlistx)
+    cn.commit()
+    #print(mlist)
+
+
+
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} {bot.user.id}")
+    #y.start()
+    data_send.start()
+
+@tasks.loop(seconds=10)
+async def data_send():
+    get_members()
+    r=requests.post("http://192.168.0.102:4402/webhook/leaderboardoikjrngWPOIURGBLREGOVCQNHREWQLIGFPWQOHRUCGTEQR", headers={'Content-Type':'application/json'}, data=mlist)
+    #print(r)
+
+@commands.cooldown(rate=1, per=3600, type=commands.BucketType.user)
+@bot.command()
+async def vote(ctx,user: discord.Member,x: str):
+    if user == ctx.author:
+        ctx.command.reset_cooldown(ctx)
+        await ctx.send("this is you, you cant")
+    elif user.bot:
+        ctx.command.reset_cooldown(ctx)
+        await ctx.send("this is bot...")
+        
+    else:
+        cur.execute(f"select score from lb where id={user.id}")
+        d=cur.fetchone()[0]
+        if x == "+":
+            f=d+1
+        elif x == "-":
+            f=d-1
+        else:
+            ctx.command.reset_cooldown(ctx)
+            raise discord.ext.commands.ArgumentParsingError("Wrong arguments.\nUsage: `-vote @member +/-`\nFor example:`-vote <@784785353533554688> -`")
+        cur.execute(f"update lb set score={f} where id={user.id}")
+        print('pass')
+        await ctx.send("voted")
+    cn.commit()
+
+
+
+
+@vote.error
+async def v_error(ctx, error):
+    if isinstance(error, discord.ext.commands.CommandOnCooldown):
+        msg = 'This command is ratelimited, please try again in {:.0f}m'.format((error.retry_after/60))
+        await ctx.send(f"`{msg}`")
+    elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
+        await ctx.send(f"Not enough arguments.\nUsage: `-vote @member +/-`\nFor example:`-vote <@784785353533554688> -`")
+    elif isinstance(error, discord.ext.commands.ArgumentParsingError):
+        await ctx.send(error)
+    else:
+        raise error
+
+@bot.command()
+async def test(ctx):
+    await ctx.send(bot.guilds)
+
+
+
+
+
+bot.run(token)
+d=input()
+if d == "":
+    os.kill(0)
