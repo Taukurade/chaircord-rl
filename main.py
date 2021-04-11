ba81e8392a065c6451c384a7506bd80d0df5309d
Airi Taukurade <34795565+Taukurade@users.noreply.github.com>
2021-04-11 16:15:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ def hello_world():
     return render_template('index.html',members=members)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=80)
+    app.run(host="0.0.0.0",port=80)
